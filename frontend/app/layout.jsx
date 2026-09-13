4bf4405c18e1382066efdc00e650a684a6028148
boodledog <gamerbay2017@gmail.com>
2026-09-13 20:17:38 +1000

    Amended code based on requests in PR

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,5 +1,5 @@
 import SiteFooter from "@/components/SiteFooter";
-import SiteHeader from "@/components/SiteHeader";
+import SiteHeader from "../components/SiteHeader";
 import "./globals.css";
 
 export const metadata = {
